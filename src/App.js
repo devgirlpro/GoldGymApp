4b9f8225258cdfe0d5fab9340125c56f18376cfc
devgirl1992 <azadeh.galidari.1992@gmail.com>
2022-09-05 18:45:04 +0200

    components-pages-routes, structure done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 import "./App.css";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <Box width="400px">
-      <Routes>
-        <Route></Route>
+      <Navbar />
+      <BrowserRouter><Routes>
+        <Route path="/" element={<Home />} />
+        <Route patgh="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
-
+      </BrowserRouter><Footer />
     </Box>
-  )
+  );
 };
 
 export default App;
