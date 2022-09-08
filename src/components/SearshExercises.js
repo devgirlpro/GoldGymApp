3407124f79d3009f0c2614b6eeb46ef84742ab6f
devgirl1992 <azadeh.galidari.1992@gmail.com>
2022-09-08 18:38:51 +0200

    get data from rapidapi & functionaliti for searching exercises is done

--- src/components/SearshExercises.js
+++ src/components/SearshExercises.js
@@ -1,27 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Button, Stack, TextField, textField, Typography } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { fetchData, exerciseOptions } from "../utils/fetchData";
 
 const SearshExercises = () => {
+  const [search, setSearch] = useState("");
+  const [exercises, setExercises] = useState([]);
+
+  //using fetched api from Rapid api
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
+      console.log(exercisesData);
+
+      const searchExercises = exercisesData.filter((exercise) => {
+        return (
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
+        );
+      });
+      console.log(searchExercises);
+
+      setSearch("");
+      setExercises(searchExercises);
+    }
+  };
+
   return (
-   <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="49px"
+        textAlign="center"
+      >
         Awesome Exercises You <br /> Should Know
       </Typography>
       <Box position="relative" mb="72px">
-        <TextField 
-        height="76px"
-        sx={{ input: { fontWeight: "700px", border: "none", borderRadius: "4px" }, width: { lg: "1000px", xs: "300px" }, backgroundColor: '#fff', borderRadius: '40px' }}
-        // value={}
-        // onChange={(e) => {}}
-        placeholder="Search Exercises"
-        type="text"
+        <TextField
+          height="76px"
+          sx={{
+            input: { fontWeight: "700px", border: "none", borderRadius: "4px" },
+            width: { lg: "1000px", xs: "300px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value.toLowerCase());
+          }}
+          placeholder="Search Exercises"
+          type="text"
         />
-        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0', fontSize: { lg: '20px', xs: '14px' } }} >Search</Button>
-
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "173px", xs: "80px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
       </Box>
+    </Stack>
+  );
+};
 
-   </Stack>
-  )
-}
-
-export default SearshExercises
+export default SearshExercises;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,18 @@
+//exerciseOption getting from RapidApi
+//key in in .env file
+export const exerciseOptions = {
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPart/back",
+  headers: {
+    "X-RapidAPI-Key": "b0f477993dmsh21ab40918948518p16575fjsn91f4cd432178",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+//fetching API
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = response.json();
+
+  return data;
+};
